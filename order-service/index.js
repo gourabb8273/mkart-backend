d2e4fcd801aa75cbd956c6ea05bc09d85c8497af
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2025-01-23 17:01:09 +0530

    initial

---
+++ order-service/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+
+app.get("/order", (req, res) => {
+    res.json({ message: "Hello from Order Service!" });
+});
+
+app.listen(port, () => {
+    console.log(`Order Service running on port ${port}`);
+});
