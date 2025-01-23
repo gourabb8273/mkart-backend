52a38896038fda07ae6dafd8519b843e03115d61
gourabb8273 <109871120+gourabb8273@users.noreply.github.com>
2025-01-23 21:34:07 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/user", (req, res) => {
-    res.json({ message: "Hello from User Service........!" });
+    res.json({ message: "Hello from User Service.........!" });
 });
 
 app.listen(port, () => {
