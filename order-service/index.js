94f4ead58847966e80f0626d202fd4c8a56895c0
gourabb8273 <109871120+gourabb8273@users.noreply.github.com>
2025-01-24 13:39:00 +0530

    Update index.js

--- order-service/index.js
+++ order-service/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/order", (req, res) => {
-    res.json({ message: "Hello from Order Service123!" });
+    res.json({ message: "Hello from Order Service1234!" });
 });
 
 app.listen(port, () => {
