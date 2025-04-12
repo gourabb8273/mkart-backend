8540bb0e2f69469a096ebc71899ba1a8cac86b0d
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2025-04-13 01:19:32 +0530

    adding wishlist route

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -67,7 +67,7 @@ const startServer = async () => {
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port  ${PORT}`);
       console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
     });
 
