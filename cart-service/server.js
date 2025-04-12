9dd9172374279529f5bc4eda906a6d3a6c3a40d4
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2025-04-12 14:45:37 +0530

    adding task definition in cart service

--- cart-service/server.js
+++ cart-service/server.js
@@ -6,7 +6,9 @@ import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './src/config/swagger.js';
 import cartRoutes from './src/routes/cartRoutes.js';
+const connectDB = require('./src/config/db'); 
 
+connectDB();
 
 const app = express();
 
@@ -14,17 +16,17 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected for Order Service'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('MongoDB connected for Order Service'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Order Service!');
+  res.send('Welcome to the Cart Service!');
 });
 
 app.use('/cart', cartRoutes);
@@ -33,7 +35,7 @@ app.use('/cart', cartRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Start server
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
   console.log(`Cart Service running on port ${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
--- cart-service/src/config/db.js
+++ cart-service/src/config/db.js
@@ -1,23 +1,31 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import getSecret from '../services/getSecret.js';
 
-const uri = process.env.MONGO_URI;
-if (!uri) {
-  console.error("Missing MONGO_URI in environment variables");
-  process.exit(1);
-}
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const getSecret = require('../services/getSecret');
+
+dotenv.config();
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri, {
+    const uriBase = await getSecret('MONGO_URI');
+    const dbName = 'product-cart'; 
+    const fullUri = `${uriBase}/${dbName}?retryWrites=true&w=majority`;
+    console.log('[MongoDB] Connecting to:', fullUri);
+    await mongoose.connect(fullUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${mongoose.connection.host}`);
+
+    console.log(`[MongoDB] Connected successfully to host: ${mongoose.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
+    console.error('[MongoDB] Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
+// export default connectDB;
