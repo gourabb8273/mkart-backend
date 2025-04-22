0499628ccbb5819fb97e1c544996246225f3cb54
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2025-04-22 18:31:04 +0530

    trigger product service

--- product-service/server.js
+++ product-service/server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(cors());
 
 app.use(cors({
-  origin: '*', 
+  origin: '*',  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,  
